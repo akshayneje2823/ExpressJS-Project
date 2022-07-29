bab318779523dc519c3ff8f261f995790e1bfe9c
Akshay Neje <akshayneje2823@gmail.com>
2022-07-29 20:11:25 +0530

    Done

---
+++ BMI Calculator/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyPrase = require('body-parser')
+
+const app = express();
+app.use(bodyPrase.urlencoded({extended:true}));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+app.post('/',function(req,res){
+
+    var weight = Number(req.body.weight)
+    var height = Number(req.body.height)
+
+    var ans = weight + height
+
+    res.send("Your BMI is " +ans)
+})
+app.listen(3000,function(){
+    console.log("we are on....")
+})
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const bodyparser = require('body-parser')
 
 const app = express();
+app.use(bodyparser.urlencoded({extended:true}))
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html")
 })
+
+app.post("/",function(req,res){
+//   console.log(req.body.num1)
+    var num1 = Number(req.body.num1)
+    var num2 = Number(req.body.num2)
+
+    var result = num1 + num2;
+
+    res.send("The answer is "+ result)
+});
 
 app.listen(3000,function(){
     console.log("Server Running On Port 3000.....")
